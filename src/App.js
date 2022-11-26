e8c811a9860c3ebc99cc8be4ab2609d0f96a3b9f
Razvy Cretu <razvan.cretu97@gmail.com>
2022-11-26 03:56:47 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Burger1 from "./components/Burgers/Burger1";
+import Burger2 from "./components/Burgers/Burger2";
+import Burger3 from "./components/Burgers/Burger3";
+import Burger4 from "./components/Burgers/Burger4";
+import Dropdown1 from "./components/Dropdowns/Dropdown1";
+import Dropdown2 from "./components/Dropdowns/Dropdown2";
+import Container from "./components/Container";
+import styles from "./styles/App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Container>
+        <Burger1 />
+      </Container>
+      <Container>
+        <Burger2 />
+      </Container>
+      <Container>
+        <Burger3 />
+      </Container>
+      <Container>
+        <Burger4 />
+      </Container>
+      <Container>
+        <Dropdown1 />
+      </Container>
+      <Container>
+        <Dropdown2 />
+      </Container>
+      <Container></Container>
+      <Container></Container>
     </div>
   );
 }
