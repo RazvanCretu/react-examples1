ec1bf9bf0448e7058a50f14812f6039b54507689
Razvy Cretu <razvan.cretu97@gmail.com>
2022-11-27 22:40:53 +0200

    Transitions on Component unmounting the DOM

--- src/components/Dropdowns/Dropdown2.js
+++ src/components/Dropdowns/Dropdown2.js
@@ -6,10 +6,11 @@ import {
   VscSignOut,
   VscArrowDown,
 } from "react-icons/vsc";
+import { CSSTransition } from "react-transition-group";
 
 import styles from "../../styles/Dropdowns/Dropdown2.module.css";
 
-const DropdownMenu = ({ children, className, handleClickOutside }) => {
+const Menu = ({ children, handleClickOutside, menuRef }) => {
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
 
@@ -18,26 +19,31 @@ const DropdownMenu = ({ children, className, handleClickOutside }) => {
     };
   }, [handleClickOutside]);
 
-  return <ul className={className}>{children}</ul>;
+  return (
+    <ul className={styles.Menu} ref={menuRef}>
+      {children}
+    </ul>
+  );
 };
 
 const Dropdown2 = ({ isOpen, handleSwitch }) => {
   const containerRef = useRef(null);
+  const menuRef = useRef(null);
 
   const handleClickOutside = (e) => {
-    if (containerRef.current && !containerRef.current.contains(e.target)) {
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(e.target) &&
+      isOpen
+    ) {
       handleSwitch();
     }
   };
 
-  const animate = (i) =>
-    `${i % 2 === 0 ? "rotRight" : "rotLeft"} .5s ease-in ${0.25 * i}s forwards`;
+  const animate = (i) => `${0.1 * i}s`;
 
   return (
-    <div
-      className={`${styles.Wrapper} ${isOpen ? styles.open : ""}`}
-      ref={containerRef}
-    >
+    <div className={styles.Wrapper} ref={containerRef}>
       <button className={styles.Button} onClick={handleSwitch}>
         <span>
           <VscArrowDown />
@@ -45,37 +51,45 @@ const Dropdown2 = ({ isOpen, handleSwitch }) => {
         Menu
       </button>
 
-      {isOpen ? (
-        <DropdownMenu
-          className={styles.Menu}
-          handleClickOutside={handleClickOutside}
-        >
-          <li style={{ animation: animate(0) }}>
+      <CSSTransition
+        nodeRef={menuRef}
+        in={isOpen}
+        timeout={1300}
+        unmountOnExit
+        classNames={{
+          enterActive: styles.enter,
+          enterDone: styles.enterActive,
+          exit: styles.exit,
+          exitActive: styles.exitActive,
+        }}
+      >
+        <Menu handleClickOutside={handleClickOutside} menuRef={menuRef}>
+          <li style={{ animationDelay: isOpen ? animate(0) : animate(3) }}>
             <span>
               <VscAccount />
             </span>
             Profile
           </li>
-          <li style={{ animation: animate(1) }}>
+          <li style={{ animationDelay: isOpen ? animate(1) : animate(2) }}>
             <span>
               <VscTools />
             </span>
             Build Tools
           </li>
-          <li style={{ animation: animate(2) }}>
+          <li style={{ animationDelay: isOpen ? animate(2) : animate(1) }}>
             <span>
               <VscSettingsGear />
             </span>
             Settings
           </li>
-          <li style={{ animation: animate(3) }}>
+          <li style={{ animationDelay: isOpen ? animate(3) : animate(0) }}>
             <span>
               <VscSignOut />
             </span>
             Log Out
           </li>
-        </DropdownMenu>
-      ) : null}
+        </Menu>
+      </CSSTransition>
     </div>
   );
 };
