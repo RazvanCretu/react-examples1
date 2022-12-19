480a5d7650728e078d2a60a4594be7544b1da313
Razvy Cretu <razvan.cretu97@gmail.com>
2022-12-19 20:02:31 +0200

    import `CSSBaseLine` and defining `GlobalStyles`

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 
+import { GlobalStyles } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.Fragment>
+    <CssBaseline />
+    <GlobalStyles styles={{ body: { backgroundColor: "#1f1f2d" } }} />
+    <App />
+  </React.Fragment>
+);
